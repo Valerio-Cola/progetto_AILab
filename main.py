f0b63b927d681661f8445442346aab5a6fa33cf1
Valerio-Cola <valeriocola2003@gmail.com>
2025-06-15 17:08:14 +0200

    Elaborato Completato

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ torch.set_grad_enabled(False)
 # Utilizziamo il protocollo TCP con la libreria socket per lo scambio di messaggi su porta 5005
 # tra PC e Raspberr, TCP è migliore di UDP perchè garantisce l'integrità e l'ordine dei messaggi
 ip = sys.argv[1]
+#ip = "192.168.160.79"
 port = 5005
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((ip, port))
@@ -43,6 +44,7 @@ s.settimeout(0.2)
 
 # Video stream da porta 8080 del Raspberry Pi Zero 2 W 
 url = f"http://{ip}:8080"
+#url = "http://192.168.160.79:8080"
 cap = cv2.VideoCapture(url)
 
 # Variabili condivise
@@ -60,6 +62,8 @@ classes = ['Pedestrians', 'green_light', 'red_light', 'speed_limit_20', 'speed_l
 
 prev_objects = None
 counter_persistance = 20
+counter_stop = -1
+flag_pedestrian = False
 
 # Flag per segnale di stop
 flag_start = False
@@ -70,11 +74,12 @@ lock = threading.Lock()
 flag_stop = False
 
 import math
-fov_deg = 62.2
+fov_deg = 50
 fov_rad = math.radians(fov_deg)
-width_px = 960
+width_px = 1280
 focal_length_px = (width_px * 0.5) / math.tan(fov_rad * 0.5)
-H_real = 7
+# Altezza reale dell'oggetto in cm, utilizzata per calcolare la distanza
+H_real = None
 
 # Variabili per la correzione della traiettoria, necessarie per evitare 
 # uno spam inutile di comandi equivalenti
@@ -131,7 +136,7 @@ def detection_worker():
         best_conf = 0
         for idx, (x1, y1, x2, y2, conf, cls) in enumerate(results.xyxy[0]):
             confidence = float(conf)  
-            if confidence > best_conf and confidence > 0.4:
+            if confidence > best_conf and confidence > 0.2:
                 best_obj = idx
                 best_conf = confidence
 
@@ -179,7 +184,7 @@ while True:
 
 
         center_right = 1000
-        threshold_intensità_colore = 100
+        threshold_intensità_colore = 80
 
         sx = 0
         dx = 0
@@ -238,43 +243,85 @@ while True:
         cv2.line(frame, (1000, 510), (1090, 510), (255,255,255), 2)
 
         # Se è stato rilevato un oggetto
-        if current_detections:
+        if counter_stop > 0:
+            counter_stop -= 1
+            print("Contatore stop:", counter_stop)
+        elif counter_stop == 0:
+            counter_stop = -1
+            flag_start = True
+            correzine_attuale = None
+            correzione_precedente = "Centro"
+            s.sendall(b'Max 20\r\n')
+            try:
+                print("RPI→", s.recv(1024).decode().strip())
+            except socket.timeout:
+                print("Nessuna risposta, msg non ricevuto")
 
+
+        if current_detections and counter_stop == -1:
+
+            
             x1,y1,x2,y2, actual_class, confidence = current_detections[0]
            
             height_pixel = y2 - y1
+            if actual_class == "Pedestrians":
+                H_real = 7.5
+            elif actual_class == "red_light" or actual_class == "green_light":
+                H_real = 3.5
+            else:
+                H_real = 5
             distance_cm = (H_real * focal_length_px) / height_pixel
-            cv2.putText(frame, f"Distanza: {distance_cm:.2f} cm", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
             # Controllo se la classe è cambiata rispetto alla precedente
             # Se la classe è cambiata resetto il contatore di persistenza
             if actual_class != prev_objects:
-                counter_persistance = 30
+                counter_persistance = 100
                 print("Classe rilevata:", actual_class, "Confidenza:", confidence)
                 prev_objects = actual_class
             
             # Se la classe è una di quelle che ci interessano e la distanza è entro i limiti di tolleranza invia i comandi
-            if((actual_class == 'stop' or actual_class == "Pedestrians" or actual_class == "red_light") and flag_start == True and distance_cm < 20 and distance_cm > 16):
+            if(actual_class == 'stop' and flag_start == True and distance_cm < 35 and distance_cm > 30):
                 s.sendall(b'Stop Rilevato\r\n') 
+                flag_start = False
+                counter_stop = 1000
+                try:
+                    print("RPI→", s.recv(1024).decode().strip())
+                except socket.timeout:
+                    print("Nessuna risposta, msg non ricevuto")
+
+            elif(actual_class == "Pedestrians" and flag_start == True and distance_cm < 25 and distance_cm > 20):
+                s.sendall(b'Pedone Rilevato\r\n')
+                flag_pedestrian = True
                 flag_start = False
                 try:
                     print("RPI→", s.recv(1024).decode().strip())
                 except socket.timeout:
                     print("Nessuna risposta, msg non ricevuto")
-            elif(actual_class == "speed_limit_20" and flag_start == True and distance_cm < 29 and distance_cm > 16):
+
+            elif(actual_class == "speed_limit_20" and flag_start == True and distance_cm < 35 and distance_cm > 30):
                 s.sendall(b'Max 20\r\n')
                 try:
                     print("RPI→", s.recv(1024).decode().strip())
                 except socket.timeout:
                     print("Nessuna risposta, msg non ricevuto")
-            elif(actual_class == "speed_limit_50" and flag_start == True and distance_cm < 29 and distance_cm > 19):
+            elif(actual_class == "speed_limit_50" and flag_start == True and distance_cm < 35 and distance_cm > 30):
                 s.sendall(b'Max 50\r\n')
                 try:
                     print("RPI 2→", s.recv(1024).decode().strip())
                 except socket.timeout:
                     print("Nessuna risposta, msg non ricevuto")
-            elif(actual_class == "green_light" and flag_start == True and distance_cm < 23 and distance_cm > 19):
-                s.sendall(b'Semaforo Verde\n')
+            elif(actual_class == "green_light" and distance_cm < 33 and distance_cm > 10):
+                flag_start = True
+                correzine_attuale = None
+                correzione_precedente = "Centro"
+                s.sendall(b'Semaforo Verde\r\n')
+                try:
+                    print("RPI→", s.recv(1024).decode().strip())
+                except socket.timeout:
+                    print("Nessuna risposta, msg non ricevuto")
+            elif(actual_class == "red_light" and flag_start == True and distance_cm < 40 and distance_cm > 32):
+                flag_start = False
+                s.sendall(b'Semaforo Rosso\r\n')
                 try:
                     print("RPI→", s.recv(1024).decode().strip())
                 except socket.timeout:
@@ -282,7 +329,7 @@ while True:
 
             
             cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
-            cv2.putText(frame, f"{actual_class} {confidence:.2f}", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(frame, f"{actual_class} {confidence:.2f} {distance_cm:.2f} ", (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         
         #Faccio un check per evitare falsi
         # Se non è stata rilevata alcuna classe decremento il timer di tolleranza, altrimenti resetto il timer e segna l'effettiva assenza di classi    
@@ -291,6 +338,19 @@ while True:
             if prev_objects is not None and counter_persistance == 0:
                 print("Nessuna classe rilevata")
                 prev_objects = None
+                if flag_pedestrian:
+                    flag_pedestrian = False
+                    flag_start = True
+                    correzine_attuale = None
+                    correzione_precedente = "Centro"
+                    s.sendall(b'GO\r\n')
+                    try:
+                        print("RPI→", s.recv(1024).decode().strip())
+                    except socket.timeout:
+                        print("Nessuna risposta, msg non ricevuto")
+
+
+                
 
         cv2.imshow("YOLOv5 Stream", frame)
 
